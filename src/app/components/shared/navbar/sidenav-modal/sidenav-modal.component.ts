72a4f3134f5e1f1b3305571d91e4fa8dfab3ef07
Miguel Romero <miguelesvida@gmail.com>
2019-11-28 01:39:22 -0600

    sidenav finish

--- src/app/components/shared/navbar/sidenav-modal/sidenav-modal.component.ts
+++ src/app/components/shared/navbar/sidenav-modal/sidenav-modal.component.ts
@@ -9,6 +9,13 @@ import { ModalService } from "../../../../services/modal.service";
 export class SidenavModalComponent implements OnInit {
   public isQuickActive: boolean = false;
   public isUniversityActive: boolean = false;
+  public isAdmissionsActive: boolean = false;
+  public isAcademicsActive: boolean = false;
+  public isResearchActive: boolean = false;
+  public isGraduatesActive: boolean = false;
+  public isInternationalActive: boolean = false;
+  public isMediaActive: boolean = false;
+
   public isSideNavActive: boolean = false;
 
   constructor(private modalService: ModalService) {
@@ -33,12 +40,50 @@ export class SidenavModalComponent implements OnInit {
   }
 
   public openSubMenu(instruction: string): void {
+    let last = {
+      quick: this.isQuickActive,
+      university: this.isUniversityActive,
+      admissions: this.isAdmissionsActive,
+      academics: this.isAcademicsActive,
+      research: this.isResearchActive,
+      graduates: this.isGraduatesActive,
+      international: this.isInternationalActive,
+      media: this.isMediaActive
+    };
+
+    this.isQuickActive = false;
+    this.isUniversityActive = false;
+    this.isAdmissionsActive = false;
+    this.isAcademicsActive = false;
+    this.isResearchActive = false;
+    this.isGraduatesActive = false;
+    this.isInternationalActive = false;
+    this.isMediaActive = false;
+
     switch (instruction) {
       case "quick":
-        this.isQuickActive = !this.isQuickActive;
+        this.isQuickActive = !last.quick;
         break;
       case "university":
-        this.isUniversityActive = !this.isUniversityActive;
+        this.isUniversityActive = !last.university;
+        break;
+      case "admissions":
+        this.isAdmissionsActive = !last.admissions;
+        break;
+      case "academics":
+        this.isAcademicsActive = !last.academics;
+        break;
+      case "research":
+        this.isResearchActive = !last.research;
+        break;
+      case "graduates":
+        this.isGraduatesActive = !last.graduates;
+        break;
+      case "international":
+        this.isInternationalActive = !last.international;
+        break;
+      case "media":
+        this.isMediaActive = !last.media;
         break;
     }
   }
