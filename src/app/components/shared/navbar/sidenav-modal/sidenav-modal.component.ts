a55f53b5c4c75873e7aa7482663d0ef5244efb31
Miguel Romero <miguelesvida@gmail.com>
2019-11-27 23:36:04 -0600

    sidenav animated

--- src/app/components/shared/navbar/sidenav-modal/sidenav-modal.component.ts
+++ src/app/components/shared/navbar/sidenav-modal/sidenav-modal.component.ts
@@ -1,33 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalService } from '../../../../services/modal.service';
+import { Component, OnInit } from "@angular/core";
+import { ModalService } from "../../../../services/modal.service";
 
 @Component({
-  selector: 'app-sidenav-modal',
-  templateUrl: './sidenav-modal.component.html',
-  styleUrls: ['./sidenav-modal.component.scss']
+  selector: "app-sidenav-modal",
+  templateUrl: "./sidenav-modal.component.html",
+  styleUrls: ["./sidenav-modal.component.scss"]
 })
 export class SidenavModalComponent implements OnInit {
+  public isQuickActive: boolean = false;
+  public isUniversityActive: boolean = false;
+  public isSideNavActive: boolean = false;
 
-  public isSideNavActive:boolean = false;
-  constructor(private modalService:ModalService) {
-    modalService.isSideNavActive.subscribe(status=>{
+  constructor(private modalService: ModalService) {
+    modalService.isSideNavActive.subscribe(status => {
       this.isSideNavActive = status;
-    })
+    });
   }
 
   ngOnInit() {
-    setTimeout(()=>{
-      let nav = document.getElementById('side-nav-modal')
-      nav.classList.add('activated');
-    },5)
+    setTimeout(() => {
+      let nav = document.getElementById("side-nav-modal");
+      nav.classList.add("activated");
+    }, 5);
   }
 
-  public deactivateSideNav():void {
-    let nav = document.getElementById('side-nav-modal')
-    nav.classList.remove('activated');
-    setTimeout(()=>{
+  public deactivateSideNav(): void {
+    let nav = document.getElementById("side-nav-modal");
+    nav.classList.remove("activated");
+    setTimeout(() => {
       this.modalService.isSideNavActive.next(!this.isSideNavActive);
-    },300)
+    }, 300);
   }
 
+  public openSubMenu(instruction: string): void {
+    switch (instruction) {
+      case "quick":
+        this.isQuickActive = !this.isQuickActive;
+        break;
+      case "university":
+        this.isUniversityActive = !this.isUniversityActive;
+        break;
+    }
+  }
 }
